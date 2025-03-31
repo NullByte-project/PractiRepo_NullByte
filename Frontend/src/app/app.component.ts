b673d0e1a937ab94b75e6f829f74f05dbb95f375
THE FABI7 <fabianguancha13@gmail.com>
2025-03-31 09:56:04 -0500

    add inicio and pie-pagina components with styles and update app structure

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EncabezadoComponent } from "./publico/pagina-maestra/encabezado/encabezado.component";
+import { InicioComponent } from "./publico/inicio/inicio.component";
+import { PiePaginaComponent } from "./publico/pagina-maestra/pie-pagina/pie-pagina.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EncabezadoComponent],
+  imports: [RouterOutlet, EncabezadoComponent, InicioComponent, PiePaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
